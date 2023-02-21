d4d56f88f6ab8f2455ee0f80a8b3f7e024783930
cetmoe <6438758+cetmoe@users.noreply.github.com>
2023-02-21 22:38:54 +0100

    Removed Vite boilerplate

---
+++ src/header/header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className='container header'>
+      <div className='logo'>MindMe</div>
+      <div className='container options'>
+        <div>Login</div>
+        <div>Search</div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
